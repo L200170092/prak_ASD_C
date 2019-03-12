a23b75316ccbbc3160bc38e73dfa387833d3abaa
L200170092 <34917350+L200170092@users.noreply.github.com>
2019-03-12 20:25:53 +0700

    Add files via upload

---
+++ Modul 2/Nomer 03.py
@@ -0,0 +1,6 @@
+#no3
+a = input("masukkan nama mahasiswa:")
+b = input("masukkan NIM mahasiswa:")
+c = input("masukkan kota tempat Tinggal mahasiswa:")
+d = input("masukkan Uang Saku mahasiswa:")
+e = print(a,b,c,d)
